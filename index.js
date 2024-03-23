0c8c08c0ae73a0886a1c966c28e2fbbf529a42a9
Maureen Cheptoo <cheptoomaureen31@gmail.com>
2024-03-23 23:01:24 +0300

    modified assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, name) {
+    let emptyArrays =[];
+    for (let i= 0; i < array.length; i++) {
+      let emptyArray =`Thank you, ${array[i]}, for the wonderful surprise gift!`;
+      emptyArrays.push(emptyArray);
+      debugger;
+    }
+    return emptyArrays;
+  }
+  function countDown(num) {
+    while (num>=0) {
+      console.log(num--);
+    }
+  }
+  console.log(countDown(10))
